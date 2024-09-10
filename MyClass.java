0fa7bfa066a3f27d4adac6929afd1550162d48f0
MeteKat <metekatiyim@hotmail.com>
2024-09-10 17:30:31 +0300

    fixed problems when capital letters sended

--- MyClass.java
+++ MyClass.java
@@ -5,52 +5,103 @@ import java.util.Scanner;
 
 public class MyClass {
 
-	public static List<Integer> convertToASCII(String message, int n, int e) {
-		List<Integer> numberValues = new ArrayList<>();
+	private static int p = 5;
+	private static int q = 19;
+	private static int n = p * q;
+	private static int e;
+	private static int d;
+
+
+	public static long modPow(long number, long power, long mod){
+		long result = 1;
+		while (power > 0) {
+			if (power % 2 == 1) {
+				result = (result * number) % mod;
+			}
+			power = power / 2;
+			number = (number * number) % mod;
+		}
+		return result;
+	}
+	
+
+	public static int gcd(int a, int b){
+		if(b == 0){
+			return a;
+		}
+		return gcd(b, a % b);
+	}
+
+	public static void findNumberE(){
+		int r = (p - 1) * (q - 1);
+		for(int i = 2; i < r; i++){
+			if(gcd(i, r) == 1){
+				e = i;
+				break;
+			}
+		}
+	}
+
+	public static void findNumberD(){
+		int i = 0;
+		int r = (p - 1) * (q - 1);
+		int numb = 0;
+		int res = 0;
+		while(true){
+			numb = (i * r) + 1;
+			if(numb % e == 0){
+				res = numb / e;
+				break;
+			}
+			i++;
+		}
+		d = res;
+	}
+
+	public static List<Double> encryptMessageList(String message) {
+		List<Double> numberValues = new ArrayList<>();
 		for (int i = 0; i < message.length(); i++) {
 			char character = message.charAt(i);
-			int number = (int) character - 64;
-			int encryptedNumber = (int) (Math.pow(number, e) % n);
+			double encryptedNumber = modPow((long) character - 32, e, n);
 			numberValues.add(encryptedNumber);
 		}
 		return numberValues;
 	}
-	public static String decryptMessageList(List<Integer> message, double n, double d) {
+
+	public static String decryptMessageList(List<Double> message) {
 		String decryptedMessage = "";
 		for (int i = 0; i < message.size(); i ++) {
-			String number = message.get(i).toString();
-			int numberInt = Integer.parseInt(number);
-			String decryptedNumber = new DecimalFormat("0").format(Math.pow(numberInt, d) % n);
-			decryptedMessage += (char) (Integer.parseInt(decryptedNumber) + 64);
+			double numberInt = message.get(i);
+			double decryptedNumber = modPow((long) numberInt, d, n);
+			String decryptedNumberString = new DecimalFormat("0").format(decryptedNumber + 32);
+			decryptedMessage += (char) (Integer.parseInt(decryptedNumberString));
 		}
 		return decryptedMessage;
 	}
-	public static List<Integer> rsaEncrypt(String messageToEncrypt) {
-		int p = 11;
-		int q = 3;
-		int n = p * q; // public key (n = 33 e = 3)
-		int e = 3;
-		int d = 7; //private key (n = 33 d = 11)
-		
 
-		List<Integer> encryptedMessageList = convertToASCII(messageToEncrypt, n, e);
+	public static List<Double> rsaEncrypt(String messageToEncrypt) {
+		findNumberE();
+		findNumberD();
 
-
+		List<Double> encryptedMessageList = encryptMessageList(messageToEncrypt);
 		return encryptedMessageList;
+		
 	}
-	public static String rsaDecrypt(List<Integer> encryptedMessageList, double n, double e){
 
-		String decryptedMessage = decryptMessageList(encryptedMessageList, n, e);
+	public static String rsaDecrypt(List<Double> encryptedMessageList){
+
+		String decryptedMessage = decryptMessageList(encryptedMessageList);
 		return decryptedMessage;
 	}
+
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter a message: ");
+		System.err.println("Enter the message to be encrypted: ");
 		String message = scanner.nextLine();
 		System.out.println("Sended Message: " + message);
-		List<Integer> encryptedMessage = rsaEncrypt(message);
-		String decryptedMessage = rsaDecrypt(encryptedMessage, 33, 7);
+		List<Double> encryptedMessage = rsaEncrypt(message);
+		String decryptedMessage = rsaDecrypt(encryptedMessage);
 		System.out.println("Decrypted Message: " + decryptedMessage);
 		scanner.close();
 		
